768a321015d742e2bed5c82d0e9f0618a760b924
Jonas Wagner <jwagner@raptus.com>
2012-11-02 13:35:09 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0b1'
 setup(name='raptus.workflownotification',
       version=version,
       description="Pluggable system to provide a way to send custom notifications to predefined recipients on a workflow transition",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='raptus.workflownotification',
       keywords='plone workflow notification',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.workflownotification',
+      url='https://github.com/Raptus/raptus.workflownotification',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus'],
